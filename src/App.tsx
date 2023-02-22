3dc34d05351b88bff74e6b05edccc1ef8391efb6
Lucas Martin-Chabalier <lucas.martinchabalier@gmail.com>
2023-02-22 16:36:02 +0100

    feat: test secret

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CountDown from './components/countdown/CountDown'
 function App() {
   return (
     <div className="flex flex-col h-screen bg-slate-300">
+      <p>{process.env.REACT_APP_APIKEY ?? 'toto'}</p>
       <div className="m-auto">
         <CountDown></CountDown>
       </div>
