81c67a483b6dd4397461f9299d7e40a01ff61aa1
lucactusss <lucas.martinchabalier@gmail.com>
2023-02-22 15:49:55 +0000

    Deploying to gh-pages from  @ ee54290a9314a15c8f6eb292a0f9252d312ae0ab 🚀

--- src/components/countdown/CountDown.tsx
+++
@@ -1,9 +0,0 @@
-import CountdownTimer from './CountDownTimer'
-
-function CountDown() {
-  const targetDate = new Date('2023/03/07 21:00:00').toString()
-
-  return <CountdownTimer targetDate={targetDate} />
-}
-
-export default CountDown
