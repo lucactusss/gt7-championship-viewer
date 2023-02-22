ae9229bfe0d6f2e556739aa2f2fbafa5ff525724
Lucas Martin-Chabalier <lucas.martinchabalier@gmail.com>
2023-02-22 19:11:40 +0100

    feat: better background

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import CountDown from './components/countdown/CountDown'
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-slate-300">
-      <p>{process.env.REACT_APP_APIKEY ?? 'toto'}</p>
+    <div className="flex flex-col h-screen bg-gradient-to-r from-purple-900 via-indigo-800 to-purple-900 text-white">
+      {/* <p>{process.env.REACT_APP_APIKEY ?? 'toto'}</p> for secret includes */}
       <div className="m-auto">
         <CountDown></CountDown>
       </div>
