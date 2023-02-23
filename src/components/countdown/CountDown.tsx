8caf5e3aa24e08107592bc42995f154105c6c59d
Lucas Martin-Chabalier <lucas.martinchabalier@gmail.com>
2023-02-23 10:44:45 +0100

    fix: change date

--- src/components/countdown/CountDown.tsx
+++ src/components/countdown/CountDown.tsx
@@ -1,7 +1,7 @@
 import CountdownTimer from './CountDownTimer'
 
 function CountDown() {
-  const targetDate = new Date('2023/03/07 21:00:00').toString()
+  const targetDate = new Date('2023/03/08 21:00:00').toString()
 
   return <CountdownTimer targetDate={targetDate} />
 }
